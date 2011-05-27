4365a48aba31d9e80d3fff9487046d3348898a6b
Laurent Charignon <L.charignon@gmail.com>
2011-05-27 15:25:42 +0200

    ajout du fichier jar pour accéder au sqlite

--- BD_Base/main.java
+++ BD_Base/main.java
@@ -6,7 +6,6 @@ public class main{
 public static void main (String argv[])
 {
     Bd bd = new Bd();
-    System.out.println("Bd créé à :"+bd);
     try { 
 	System.out.println(bd.returnHash(argv[0]));}
     catch (Exception e){
